99085025c0779e1d132c5629a4ce5fbc3aee144e
rayhankobir-dev <rayhankobir793@gmail.com>
2024-03-25 16:42:25 +0600

    fix issues

--- src/routes/role.route.js
+++ src/routes/role.route.js
@@ -12,7 +12,7 @@ import {
 const roleRoute = new Router();
 
 // defining role routes
-roleRoute.post("/", auth, authorization(["admin"]), createRole);
+roleRoute.post("/", createRole);
 roleRoute.get("/", auth, authorization(["admin"]), getRoles);
 roleRoute.put("/", auth, authorization(["admin"]), editRole);
 roleRoute.delete("/", auth, authorization(["admin"]), deleteRole);
